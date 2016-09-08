381b5103c21698b28c142a81305bb2c60cbb5a25
Hsu Ken Ooi <hsukenooi@gmail.com>
2016-09-08 16:05:03 +0100

    added an echo

--- app/models/message_processor.rb
+++ app/models/message_processor.rb
@@ -6,6 +6,10 @@ class MessageProcessor
 
   def do_thing
     # This gets invoked whenever a message is received
+    if @message.text # Some messages are delivery notices without text
+      reply = MessengerPlatform::TextMessage.new(@message.sender, @message.text)
+      reply.deliver
+    end
   end
 
 end
