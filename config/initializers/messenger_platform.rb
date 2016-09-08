92800201e85864ee14c6cf4d86d6b69cd88e5a29
Hsu Ken Ooi <hsukenooi@gmail.com>
2016-09-08 16:14:51 +0100

    changed to the universe facebook page instead of eggsprout

--- config/initializers/messenger_platform.rb
+++ config/initializers/messenger_platform.rb
@@ -1,6 +1,6 @@
 MessengerPlatform.setup do |config|
   config.verify_token = 'my_voice_is_my_password_verify_me'
-  config.access_token = 'EAACpsIoHrdQBAKq7Sy35WMiiLDElshZBZAjT3KJCk4SuyV50Ni3ZBcRkJibCCOTHKJ7JbAGhNkbzhmMfQoVZC53qB0syy7zJUVb0V97hp3I6ZCfHZCYGTrjR7pvYyuHLFxeGt3DpVyz0MEZB0tVjeetkY0EUiUf7UZAr8itTuXfU3QZDZD'
+  config.access_token = 'EAACpsIoHrdQBAHANVVLaYtpowqN3BxlEOs9ZCb1gCJuBOb0I1EOT5zCrAeGuqvxqa33BRmzvkG2GvtR8XurDlbsdxT0UlaIJGCt5Yazb5lit0Q7QMUP35jpBAmJKNk1MD0yEpy03AZBZAZBW2PSKfY6d1SrGCFgHAca8xZCjqpQZDZD'
 
   config.processor_class = MessageProcessor
   config.processor_method = :do_thing
